00d97c04cf78815ccf5728fa4bd70124c289ba8d
Guillermo Rebolledo <guillermo.ortiz@wizeline.com>
2021-04-08 16:07:59 -0500

    🚧 Added try/catch

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,14 +1,19 @@
 import pup from 'puppeteer';
 
 export default async function handler(req, res) {
-	const { text } = req.query;
-	const urlencodedText = encodeURIComponent(text);
-	const browser = await pup.launch();
-	const page = await browser.newPage();
-	await page.goto(`${process.env.BASE_URL}/api/image?text=${urlencodedText}`);
-	const ogImageElement = await page.$('#og-image');
-	const image = await ogImageElement.screenshot();
-	await page.close();
+	try {
+		const { text } = req.query;
+		const urlencodedText = encodeURIComponent(text);
+		const browser = await pup.launch();
+		const page = await browser.newPage();
+		await page.goto(`${process.env.BASE_URL}/api/image?text=${urlencodedText}`);
+		const ogImageElement = await page.$('#og-image');
+		const image = await ogImageElement.screenshot();
+		await page.close();
+		await browser.close();
 
-	res.send(image);
+		res.send(image);
+	} catch (e) {
+		res.send('There was an error trying to get the preview. ' + e.message);
+	}
 }
